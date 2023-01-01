e1a00f1674c445347c911227e2c421fa2e77a9e6
Hiten Vats <hitenvats16@gmail.com>
2023-01-01 18:08:25 +0530

    formated index.js

--- index.js
+++ index.js
@@ -9,27 +9,27 @@ import swaggerUIExpress from 'swagger-ui-express'
 const app = express()
 const options = {
     definition: {
-      openapi: '3.0.0',
-      info: {
-        title: 'Prep_Front',
-        version: '1.0.0',
-        description: 'Your go to Placement guide'
-      },
+        openapi: '3.0.0',
+        info: {
+            title: 'Prep_Front',
+            version: '1.0.0',
+            description: 'Your go to Placement guide'
+        },
     },
     apis: ['./routes/*.js'],
-  };
+};
 
 
 // generating api documentation based of openAPI specifications
 const openapiSpecification = swaggerJsDocs(options);
-app.use('/api-docs',swaggerUIExpress.serve,swaggerUIExpress.setup(openapiSpecification))
+app.use('/api-docs', swaggerUIExpress.serve, swaggerUIExpress.setup(openapiSpecification))
 
 
 //middlewares
 app.use(express.json())
-app.use('/user',userRoute)
+app.use('/user', userRoute)
 
 const PORT = 1234
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server Listning on port ${PORT}`)
 })
